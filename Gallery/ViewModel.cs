7d5aba94f3210025fcc187790f0927e3c3dc460b
menf <menfis666@gmail.com>
2017-06-04 13:37:13 +0200

    cos probuje zbindowac ale lipa troche

--- Gallery/ViewModel.cs
+++ Gallery/ViewModel.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
-
+using System.Windows.Forms;
 namespace Gallery
 {
     class ViewModel : INotifyPropertyChanged
     {
         public ViewModel()
         {
-            //SaveCmd = new RelayCommand(pars => Save());
+            Debug.WriteLine("INIT VIEWMODEL");
+            _canExecute = true;
         }
         public event PropertyChangedEventHandler PropertyChanged = null;
-        public ICommand AddToFavourites { get; set; }
+        private bool _canExecute;
+        private ICommand AddToFavourites { get; set; }
+
+        private ICommand _openFile;
+        public ICommand OpenFile
+        {
+            get
+            {
+                Debug.WriteLine("GET OPENFILE");
+                return _openFile ?? (_openFile = new CommandHandler(() => Open(), _canExecute));
+            }
+        }
+
         private Image _WorkspaceImage = null;
         private string _FirstName = null;
         public string FirstName
@@ -49,10 +63,53 @@ namespace Gallery
         {
             // logika odpowiedzialna za zapis
         }
+        public void Open()
+        {
+            Debug.WriteLine("OPEN");
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+            openfiledialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openfiledialog.Title = "Please select an image file.";
+            if (openfiledialog.ShowDialog() == DialogResult.OK)
+            {
+                //pozniej
+            }
+        }
+        private void Dummy()
+        {
+            // logika odpowiedzialna za cos
+        }
         virtual protected void OnPropertyChanged(string propName)
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+    }
+
+    // w stylu new CommandHandler(() => MyAction(), _canExecute))
+    public class CommandHandler : ICommand
+    {
+        private Action _action;
+        private bool _canExecute;
+        public CommandHandler(Action action, bool canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged { add { CommandManager.RequerySuggested += value; } remove { CommandManager.RequerySuggested -= value; } }
+
+        public void Execute(object parameter)
+        {
+            Debug.WriteLine("EXECUTE");
+            _action();
         }
     }
 }
